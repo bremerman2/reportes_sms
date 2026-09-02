1edde9c3963a8668355ea69c823a5f7d5ac52570
gabym3107 <gabriel.macedo@estudiantes.utec.edu.uy>
2026-09-02 18:38:44 -0300

    Update Usuario.java

--- src/main/java/org/example/Usuario.java
+++ src/main/java/org/example/Usuario.java
@@ -2,8 +2,7 @@ package org.example;
 
 public class Usuario {
     int id_usuario;
-    boolean permiso_sms;
-    String token_dispositivo;
+    boolean bloqueado;
     String nombre;
     String apellido;
     String telefono;
@@ -14,10 +13,9 @@ public class Usuario {
     public Usuario() {
     }
 
-    public Usuario(int id_usuario, boolean permiso_sms, String token_dispositivo, String nombre, String apellido, String telefono, String email, String password, Administrador administrador){
+    public Usuario(int id_usuario, boolean bloqueado, String nombre, String apellido, String telefono, String email, String password, Administrador administrador){
         this.id_usuario = id_usuario;
-        this.permiso_sms = permiso_sms;
-        this.token_dispositivo = token_dispositivo;
+        this.bloqueado = bloqueado;
         this.nombre = nombre;
         this.apellido = apellido;
         this.telefono = telefono;
@@ -26,28 +24,13 @@ public class Usuario {
         this.administrador = administrador;
     }
 
+    // Getters y Setters
     public int getId_usuario() {
         return id_usuario;
     }
 
     public void setId_usuario(int id_usuario) {
         this.id_usuario = id_usuario;
-    }
-
-    public boolean isPermiso_sms() {
-        return permiso_sms;
-    }
-
-    public void setPermiso_sms(boolean permiso_sms) {
-        this.permiso_sms = permiso_sms;
-    }
-
-    public String getToken_dispositivo() {
-        return token_dispositivo;
-    }
-
-    public void setToken_dispositivo(String token_dispositivo) {
-        this.token_dispositivo = token_dispositivo;
     }
 
     public String getNombre() {
@@ -96,5 +79,13 @@ public class Usuario {
 
     public void setAdministrador(Administrador administrador) {
         this.administrador = administrador;
+    }
+
+    public boolean isBloqueado() {
+        return bloqueado;
+    }
+
+    public void setBloqueado(boolean bloqueado) {
+        this.bloqueado = bloqueado;
     }
 }
